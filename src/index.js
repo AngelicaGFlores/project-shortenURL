e7424c0de346412f73afa11f7e80e34c020e5754
AngelicaGFlores <flores.guada.angelica@gmail.com>
2025-10-16 13:23:28 -0700

    i display list url to html

--- src/index.js
+++ src/index.js
@@ -1,16 +1,22 @@
 // Access token: 0233e996c6c7d9a40cbbf0d8c56c53fe4485f01b for bitly
+
+//Access token: c67b160bd075d24a8f3c97d05f58cf8ef9019ef0
 const longURLinput = document.getElementById(`usertextURL`);
 const submitInput = document.getElementById(`submit`);
+const listLinksUL = document.getElementById(`section-list`);
+//array of links
+const arrayLinks = [];
 
 // after user submit
 submitInput.addEventListener("click", async () => {
 	console.log(`This is the URL : ${longURLinput.value}`);
 	const longURL = longURLinput.value;
+	// try catch...
 	try {
 		const response = await fetch("https://api-ssl.bitly.com/v4/shorten", {
 			method: "POST",
 			headers: {
-				Authorization: `Bearer 0233e996c6c7d9a40cbbf0d8c56c53fe4485f01b`,
+				Authorization: `Bearer c67b160bd075d24a8f3c97d05f58cf8ef9019ef0`,
 				"Content-Type": "application/json",
 			},
 			body: JSON.stringify({
@@ -18,20 +24,36 @@ submitInput.addEventListener("click", async () => {
 				domain: "bit.ly",
 			}),
 		});
-        const data = await response.json()
-        console.log(data)
-    } catch (error) {
-        
-        console.error(error)
-    }
+
+		const data = await response.json();
+		console.log(data.link);
+		// lets add to the array
+		arrayLinks.push({
+			link: data.link,
+			long: data.long_url,
+		});
+		arrayLinks.forEach(link => {
+			//display that current array in html/css
+			let listItem = document.createElement("li");
+			//add a button, that button needs to copy short link 
+			listItem.innerHTML = `<li>${link.long} ${link.link}</li>`;
+			const button = document.createElement("button");
+			button.addEventListener("click", () => {
+				//copy the short url link to clipboard
+			
+			})
+			button.textContent = "copy";
+			listItem.appendChild(button)
+			listLinksUL.appendChild(listItem);
+		})
+
+	} catch (error) {
+		console.error(error);
+	}
 });
-
-// try catch...
-//display
-
-
-
-// be able tocopy the short link to the clip board when you click copy button 
+			
+//how to use copy to clip board search it up
+// be able to copy the short link to the clip board when you click copy button 
 
 //save to local storage using dom
 
